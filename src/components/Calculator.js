470c34eb3f79fc0bb7cc3f49635997b29535b263
Mudasir Sherwani <mudasir9757@gmail.com>
2022-10-26 11:21:58 +0500

    remove button tag and use up-lift stat for button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
+import KeyButtons from './keys';
 import './Calculator.css';
 
 export default class Calculator extends Component {
@@ -13,7 +14,7 @@ export default class Calculator extends Component {
   }
 
   EventHandler = (element) => {
-    const btnValue = element.target.textContent;
+    const btnValue = element.keyTag;
     const getCalcu = calculate(this.state, btnValue);
     this.setState(getCalcu);
   };
@@ -30,25 +31,7 @@ export default class Calculator extends Component {
           </div>
         </div>
         <div className="inputs">
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">AC</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">+/-</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">%</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-org" value="number">÷</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">7</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">8</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">9</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-org" value="number">x</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">4</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">5</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">6</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-org" value="number">-</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">1</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">2</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">3</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-org" value="number">+</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-zero" value="number">0</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn" value="number">.</button>
-          <button type="submit" onClick={this.EventHandler} className="input-btn btn-org" value="number">=</button>
+          <KeyButtons onClickOperation={this.EventHandler} />
         </div>
       </div>
     );
