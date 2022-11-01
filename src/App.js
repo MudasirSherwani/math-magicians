f696eebec2ea751c3adeba59fd85fd49d751a977
Mudasir Sherwani <mudasir9757@gmail.com>
2022-11-01 19:50:13 +0500

    Add Route in app.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
+import Quote from './components/Quote';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
         </Routes>
       </Router>
     </>
