a3c375c966a1e7973954a4496e1bb00079cad708
Mudasir Sherwani <mudasir9757@gmail.com>
2022-11-01 18:10:16 +0500

    Add Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Calculator from './components/Calculator';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Calculator from "./components/Calculator";
+import Navigation from "./components/Navigation";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/Calculator" element={<Calculator />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
