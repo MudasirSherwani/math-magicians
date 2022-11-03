683b48245f1eee24ca7144b2ec1cf3b2f05a5877
Mudasir Sherwani <mudasir9757@gmail.com>
2022-11-03 13:45:56 +0500

    fix linters

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -2,18 +2,18 @@ import operate from '../logic/operate';
 
 describe('Test Suit For Math Operations', () => {
   test('Test For Addition Of Two Numbers', () => {
-    expect(operate(4,2,'+')).toEqual('6');
+    expect(operate(4, 2, '+')).toEqual('6');
   });
 
   test('Test For Subtraction Of Two Numbers', () => {
-    expect(operate(4,2,'-')).toEqual('2');
+    expect(operate(4, 2, '-')).toEqual('2');
   });
 
   test('Test For Multiplication Of Two Numbers', () => {
-    expect(operate(4,2,'x')).toEqual('8');
+    expect(operate(4, 2, 'x')).toEqual('8');
   });
 
   test('Test For Division Of Two Numbers', () => {
-    expect(operate(4,2,'÷')).toEqual('2');
+    expect(operate(4, 2, '÷')).toEqual('2');
   });
 });
